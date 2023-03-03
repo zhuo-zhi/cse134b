55b68d1ca09b9ebc77132abe7e6d3a92a0f43830
zhuo-zhi <517770911@qq.com>
2023-03-02 19:04:00 -0800

    add search icon

---
+++ search.js
@@ -0,0 +1,14 @@
+function search_projects() {
+    let searchInput = document.getElementById('searchInput').value
+    searchInput = searchInput.toLowerCase();
+    let projects = document.getElementsByClassName('project-item');
+      
+    for (i = 0; i < projects.length; i++) { 
+        if (!projects[i].innerHTML.toLowerCase().includes(searchInput)) {
+            projects[i].style.display="none";
+        }
+        else {
+            projects[i].style.display="list-item";                 
+        }
+    }
+}
